c3c40daddb03a1f0299751446ad6dc5e3eea6c8d
HPC Desktop SEIC 404 <psukphranee@COSE-SEC404-DT1.ad.sfsu.edu>
2024-09-11 15:10:51 -0700

    removed wandb from launch script

--- training/open_cavp_main/launch_script_modified.sh
+++ training/open_cavp_main/launch_script_modified.sh
@@ -4,7 +4,7 @@
 CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 -m src.training.main_wds_intra_contrast \
 --train-data audioset_vggsound_music --val-data 'Test/vggsound-{000000..000031}.tar' --train-num-samples 567980 \
 --batch-size 1 --precision amp --workers 1 --dataset-type vggsound_audioset_music_webdataset_intra_contrast \
---model audio_contrastive_pretrained --report-to wandb --wandb-project-name audio_contrastive \
+--model audio_contrastive_pretrained \
 --name lr_8e-4_warmup200_wds_vgg+audioset_cnn14_pretrained_clip_num3_shift_lb8_intra_loss_w1 --lr 8e-4 \
 --subset_num 20000 --epochs 1 --warmup 200 --loss_type clip_intra_contrast --temporal_mix_weight 1 \
 --save-frequency 1 --spec_encode cnn14_pool --video_encode Slowonly_pool --val-frequency 2 --save-most-recent \
