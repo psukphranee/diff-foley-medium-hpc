00bc1e7970e6d9ab54ac2067f96fe7c64dd0b3b8
Panya Sukphranee <psukphranee@gmail.com>
2024-09-14 23:13:52 -0700

    da

--- training/open_cavp_main/src/training/main_wds_intra_contrast.py
+++ training/open_cavp_main/src/training/main_wds_intra_contrast.py
@@ -324,6 +324,7 @@ def main(args):
             logging.info(f"=> loaded checkpoint '{args.resume}' (epoch {start_epoch})")
 
     # initialize datasets
+    logging.info("DEBUG: Line 327")
     data = get_data(args, (preprocess_train, preprocess_val), epoch=start_epoch, tokenizer=get_tokenizer(args.model))
     assert len(data), 'At least one train or eval dataset must be specified.'
 
