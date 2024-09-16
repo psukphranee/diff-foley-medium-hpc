24ad439f7ca1bb52ba17794b7b8a962be230d0f4
HPC Desktop SEIC 404 <psukphranee@COSE-SEC404-DT1.ad.sfsu.edu>
2024-09-16 16:37:55 -0700

    modified launch script. remove val_data arg, changed train-num-sample to 500, subset-num 400, warmup 100

--- training/open_cavp_main/launch_script_modified.sh
+++ training/open_cavp_main/launch_script_modified.sh
@@ -2,10 +2,26 @@
 
 
 CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 -m src.training.main_wds_intra_contrast \
---train-data audioset_vggsound_music --val-data 'Test/vggsound-{000001..000002}.tar' --train-num-samples 32 \
---batch-size 1 --precision amp --workers 1 --dataset-type vggsound_audioset_music_webdataset_intra_contrast \
+--train-data audioset_vggsound_music \
+--train-num-samples 500 \
+--batch-size 2 \
+--precision amp \
+--workers 1 \
+--dataset-type vggsound_audioset_music_webdataset_intra_contrast \
 --model audio_contrastive_pretrained \
---name lr_8e-4_warmup200_wds_vgg+audioset_cnn14_pretrained_clip_num3_shift_lb8_intra_loss_w1 --lr 8e-4 \
---subset_num 20000 --epochs 1 --warmup 200 --loss_type clip_intra_contrast --temporal_mix_weight 1 \
---save-frequency 1 --spec_encode cnn14_pool --video_encode Slowonly_pool --val-frequency 2 --save-most-recent \
---intra_clip_num 3 --shift_lb 8 --save-most-recent --intra_contrast_weight 1
+--name lr_8e-4_warmup200_wds_vgg+audioset_cnn14_pretrained_clip_num3_shift_lb8_intra_loss_w1 \
+--lr 8e-4 \
+--subset_num 400 \
+--epochs 1 \
+--warmup 100 \
+--loss_type clip_intra_contrast \
+--temporal_mix_weight 1 \
+--save-frequency 1 \
+--spec_encode cnn14_pool \
+--video_encode Slowonly_pool \
+--val-frequency 2 \
+--save-most-recent \
+--intra_clip_num 3 \
+--shift_lb 8 \
+--save-most-recent \
+--intra_contrast_weight 1
