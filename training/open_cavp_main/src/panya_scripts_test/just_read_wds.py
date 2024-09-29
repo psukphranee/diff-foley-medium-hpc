380517a5e4d07d0f63b30f012e49fc2d4f498097
Pan Su <psukphranee@gmail.com>
2024-09-29 00:49:49 -0700

    fsfd

---
+++ training/open_cavp_main/src/panya_scripts_test/just_read_wds.py
@@ -0,0 +1,8 @@
+import webdataset as wds
+
+dataset = wds.Dataset("/Users/920753844/Diff-Foley/video/goodarchive_1.tar")
+
+sample = next(iter(dataset))
+
+for k, v in sample.items():
+    print("%20s = %s"%(k, repr(v)[:60]))
