8c9ab86d5e2f61f187ffdc38ddd68975abb80a38
Pan Su <psukphranee@gmail.com>
2024-09-29 02:33:08 -0700

    dfgbhnj

--- training/open_cavp_main/src/training/data.py
+++ training/open_cavp_main/src/training/data.py
@@ -189,7 +189,7 @@ def group_by_keys_nothrow(data, keys=base_plus_ext, lcase=True, suffixes=None, h
     current_sample = None
     for filesample in data:
         assert isinstance(filesample, dict)
-        print(f"assetion true. filesample type={type(filesample)}")
+        print(f"assetion true. filesample type={type(filesample)}. keys={filesample.keys()}")
         fname, value = filesample["fname"], filesample["data"]
         print(f"Panya: processing file {fname}")
         prefix, suffix = keys(fname)
