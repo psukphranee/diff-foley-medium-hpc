4028e7a80ac842187c542b31b228b1e8013d37d9
Pan Su <psukphranee@gmail.com>
2024-09-28 23:50:04 -0700

    update debug statements

--- training/open_cavp_main/src/training/data.py
+++ training/open_cavp_main/src/training/data.py
@@ -1520,9 +1520,9 @@ def get_wds_dataset_vggsound_audioset_music_intra_contrast(args, preprocess_img,
 
     assert input_shards is not None
     resampled = getattr(args, 'dataset_resampled', False) and is_train
-    print("Line 1521. resampled: ", str(resampled))
+    logging.info("Line 1521. resampled: ", str(resampled))
     num_samples, num_shards = get_dataset_size(input_shards)
-    logging.info("Panya: num_samples= %s, num_shards=%s", num_samples, num_shards)
+    logging.info("Panya: num_samples= %d, num_shards=%d", num_samples, num_shards)
     if not num_samples:
         if is_train:
             num_samples = args.train_num_samples
