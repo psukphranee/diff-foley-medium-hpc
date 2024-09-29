06cf7a381018de8aacc80eb7ead7f427f60cbde3
Pan Su <psukphranee@gmail.com>
2024-09-29 01:18:54 -0700

    sdfghj

--- training/open_cavp_main/src/training/data.py
+++ training/open_cavp_main/src/training/data.py
@@ -186,10 +186,12 @@ def group_by_keys_nothrow(data, keys=base_plus_ext, lcase=True, suffixes=None, h
     :param keys: function that splits the key into key and extension (base_plus_ext)
     :param lcase: convert suffixes to lower case (Default value = True)
     """
+    count = 0
     current_sample = None
     for filesample in data:
         assert isinstance(filesample, dict)
         fname, value = filesample["fname"], filesample["data"]
+        logging.info("%d: %s", count, fname)
         prefix, suffix = keys(fname)
         if prefix is None:
             continue
@@ -204,6 +206,7 @@ def group_by_keys_nothrow(data, keys=base_plus_ext, lcase=True, suffixes=None, h
             current_sample = dict(__key__=prefix, __url__=filesample["__url__"])
         if suffixes is None or suffix in suffixes:
             current_sample[suffix] = value
+        count += 1
     if valid_sample(current_sample):
         yield current_sample
 
