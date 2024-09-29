7f66587dfad18df07ddc9ce1acaa3682105ea055
Pan Su <psukphranee@gmail.com>
2024-09-29 00:33:41 -0700

    update test

--- training/open_cavp_main/src/panya_scripts_test/test_tarfile_to_samples_nothrow.py
+++ training/open_cavp_main/src/panya_scripts_test/test_tarfile_to_samples_nothrow.py
@@ -1,10 +1,11 @@
 import sys
 import os
-from data import tarfile_to_samples_nothrow  # Assuming data.py is in the parent directory
 
 # Step 1: Importing the necessary files
 # Add the parent directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../training/')))
+
+from data import tarfile_to_samples_nothrow  # Assuming data.py is in the parent directory
 
 # Step 2: Call tarfile_to_samples_nothrow with 'goodarchive_1.tar'
 tar_file_path = '/Users/920753844/Diff-Foley/video/goodarchive_1.tar'
