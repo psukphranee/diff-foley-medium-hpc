ed1791a529f61e3f449de0b585542d2225769cc9
Pan Su <psukphranee@gmail.com>
2024-10-09 23:40:35 -0700

    FutureWarning: `torch.cuda.amp.autocast(args...)` is deprecated. Please use `torch.amp.autocast('cuda', args...)` instead.

--- training/open_cavp_main/src/training/precision.py
+++ training/open_cavp_main/src/training/precision.py
@@ -4,9 +4,12 @@ from contextlib import suppress
 
 def get_autocast(precision):
     if precision == 'amp':
-        return torch.cuda.amp.autocast
+        # Panya 10.9.24 Change this per warning
+        #return torch.cuda.amp.autocast
+        return torch.amp.autocast("cuda")
     elif precision == 'amp_bfloat16' or precision == 'amp_bf16':
         # amp_bfloat16 is more stable than amp float16 for clip training
-        return lambda: torch.cuda.amp.autocast(dtype=torch.bfloat16)
+        # Panya (10.9.24) Comment out original : return lambda: torch.cuda.amp.autocast(dtype=torch.bfloat16)
+        return lambda: torch.amp.autocast("cuda", dtype=torch.bfloat16)
     else:
         return suppress
