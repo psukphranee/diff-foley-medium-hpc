e1faf8e5ea3e9e9207511617607c9c16202a9538
Pan Su <psukphranee@gmail.com>
2024-10-09 23:45:17 -0700

    revert back to original deprecated command

--- training/open_cavp_main/src/training/precision.py
+++ training/open_cavp_main/src/training/precision.py
@@ -4,12 +4,9 @@ from contextlib import suppress
 
 def get_autocast(precision):
     if precision == 'amp':
-        # Panya 10.9.24 Change this per warning
-        #return torch.cuda.amp.autocast
-        return torch.amp.autocast("cuda")
+        return torch.cuda.amp.autocast
     elif precision == 'amp_bfloat16' or precision == 'amp_bf16':
         # amp_bfloat16 is more stable than amp float16 for clip training
-        # Panya (10.9.24) Comment out original : return lambda: torch.cuda.amp.autocast(dtype=torch.bfloat16)
-        return lambda: torch.amp.autocast("cuda", dtype=torch.bfloat16)
+        return lambda: torch.cuda.amp.autocast(dtype=torch.bfloat16)
     else:
         return suppress
