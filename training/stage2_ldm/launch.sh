365823292b153171ce2f05b91f58a7339af4889e
Pan Su <psukphranee@gmail.com>
2024-11-02 21:07:06 -0700

    changed gpus in launch.sh from 0,..,7 to 0,...,4

--- training/stage2_ldm/launch.sh
+++ training/stage2_ldm/launch.sh
@@ -3,7 +3,7 @@
 python main.py \
 --base ./configs/ldm_training/diff_foley_train.yaml \
 -t --name diff_foley_experiment  \
---gpus 0,1,2,3,4,5,6,7 \
+--gpus 0,1,2,3 \
 --stage 2 \
 --epoch 250 \
 --scale_lr False
