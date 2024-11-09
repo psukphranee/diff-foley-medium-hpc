d304eadc7aa6ecdfed91fffac29fc6fda26cd346
Pan Su <psukphranee@gmail.com>
2024-11-09 13:38:32 -0500

    added dictionary of params

--- training/stage2_ldm/panya_test_script.py
+++ training/stage2_ldm/panya_test_script.py
@@ -1,3 +1,14 @@
+# Goal is to use the Stage 1 CAVP to extract features from video
+
+'''
+ Panya 11.9.24
+
+missing arguments in Decoder_Wrapper 'first_stage_config', 'decoder_config', 'scheduler_config', and 'monitor'
+
+1) first_stage_config - dictionary with keys 'target' and 'params'
+2) 
+'''
+
 # test_decoder_wrapper.py
 import torch
 import importlib.util
@@ -14,14 +25,17 @@ spec.loader.exec_module(decode_wrapper)
 # Access the Decoder_Wrapper class
 Decoder_Wrapper = decode_wrapper.Decoder_Wrapper
 
+first_stage_config = {
+    target : '/Users/920753844/Diff-Foley/training/open_cavp_main/logs/2024_10_21-14_32_19-lr_8e-4_warmup200_wds_vgg+audioset_cnn14_pretrained_clip_num3_shift_lb8_intra_loss_w1/checkpoints/epoch_latest.pt',
+    params : None
+}
+
 
 # Define a test function to check basic functionality of Decoder_Wrapper
 def test_decoder_wrapper():
     # Define dummy parameters (update with actual parameters if known)
     first_stage_model = None  # Replace with an actual model if required by the constructor
     some_param = 512  # Hypothetical parameter
-    
-    first_stage_ckpt = '/Users/920753844/Diff-Foley/training/open_cavp_main/logs/2024_10_21-14_32_19-lr_8e-4_warmup200_wds_vgg+audioset_cnn14_pretrained_clip_num3_shift_lb8_intra_loss_w1/checkpoints/epoch_latest.pt'
 
     # Instantiate the Decoder_Wrapper class
     model = Decoder_Wrapper(first_stage_model=first_stage_model, param=some_param, 
