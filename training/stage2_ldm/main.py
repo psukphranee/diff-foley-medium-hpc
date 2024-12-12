269c42555b8bc5303fc9736d079c1433154cfa5e
HPC Desktop SEIC 404 <psukphranee@gmail.com>
2024-12-11 17:03:23 -0800

    dfdsf

--- training/stage2_ldm/main.py
+++ training/stage2_ldm/main.py
@@ -641,7 +641,7 @@ if __name__ == "__main__":
     bs, base_lr = config.data.params.batch_size, config.model.base_learning_rate
     if not cpu:
         # Panya Debug 12.11.24
-        print("Panya lightning_config.trainer.gpus: ", lightning_config.trainer.gpus)
+        print("Panya lightning_config.trainer.gpus: ", type(lightning_config.trainer.gpus))
         ngpu = len(lightning_config.trainer.gpus.strip(",").split(','))
     else:
         ngpu = 1
