8150171a3738e4b79d4cd3a16c38bffafe7ff178
Pan Su <psukphranee@gmail.com>
2025-01-13 16:57:14 -0800

    jljl

--- training/stage2_ldm/CAVP_encoder.py
+++ training/stage2_ldm/CAVP_encoder.py
@@ -6,8 +6,32 @@ import os
 from open_clip.factory import get_model_config, list_models
 from adm.modules.stage2_decode.clip_video_spec import CLIP_Video_Spec_v2, CLIP_Video_Spec
 
+# loading helper functions
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+
+def instantiate_from_config(config):
+    if not "target" in config:
+        if config == '__is_first_stage__':
+            return None
+        elif config == "__is_unconditional__":
+            return None
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
 
 # initialize stage 1 model
+def instantiate_first_stage(config):
+    model = instantiate_from_config(config)
+    self.first_stage_model = model.eval()
+    self.first_stage_model.train = disabled_train
+    for param in self.first_stage_model.parameters():
+        param.requires_grad = False
+
 def init_first_from_ckpt(path):
     model = torch.load(path, map_location="cpu")
     # 12.1.24 Panya: Loaded model from checkpoint in decode_wrapper.py
@@ -19,6 +43,8 @@ def init_first_from_ckpt(path):
     for key in model.keys():
         new_key = key.replace("module.","")
         new_model[new_key] = model[key]
+
+    first_stage_model = model.eval()
 
     missing, unexpected = model.load_state_dict(new_model, strict=False)
     print(f"Restored from {path} with {len(missing)} missing and {len(unexpected)} unexpected keys")
