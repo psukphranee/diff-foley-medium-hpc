39003a23340032d3257e735c4cc277e71184c235
Pan Su <psukphranee@gmail.com>
2025-01-13 16:48:14 -0800

    fdsf

--- training/stage2_ldm/CAVP_encoder.py
+++ training/stage2_ldm/CAVP_encoder.py
@@ -3,30 +3,12 @@ import torch.nn as nn
 
 import os
 
-print("Current file path:", os.path.abspath(__file__))
-print("Parent directory:", os.path.dirname(os.path.abspath(__file__)))
-print("Root directory candidate:", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
 from open_clip.factory import get_model_config, list_models
 from adm.modules.stage2_decode.clip_video_spec import CLIP_Video_Spec_v2, CLIP_Video_Spec
 
-# load stage 1 config
-first_stage_config = get_model_config("audio_contrastive_pretrained")
-
-first_stage_config['target'] = 'adm.modules.stage2_decode.clip_video_spec.CLIP_Video_Spec_v2'
-first_stage_config['params'] = {
-    "video_encode": "Slowonly",
-    "spec_encode": "cnn14_pool",
-    "embed_dim": 512
-}
-
-# load stage 1 checkpoint
-first_stage_ckpt_path = "/Users/920753844/Diff-Foley/training/open_cavp_main/logs/2024_10_21-14_32_19-lr_8e-4_warmup200_wds_vgg+audioset_cnn14_pretrained_clip_num3_shift_lb8_intra_loss_w1/checkpoints/epoch_latest.pt"
-
 
 # initialize stage 1 model
-def init_first_from_ckpt(self, path):
+def init_first_from_ckpt(path):
     model = torch.load(path, map_location="cpu")
     # 12.1.24 Panya: Loaded model from checkpoint in decode_wrapper.py
     print("Panya: Loaded model from checkpoint in decode_wrapper.py")
@@ -45,3 +27,23 @@ def init_first_from_ckpt(self, path):
         print(f"Unexpected Keys: {unexpected}")
     
     return model
+
+def main():
+
+    # load stage 1 config
+    first_stage_config = get_model_config("audio_contrastive_pretrained")
+
+    first_stage_config['target'] = 'adm.modules.stage2_decode.clip_video_spec.CLIP_Video_Spec_v2'
+    first_stage_config['params'] = {
+        "video_encode": "Slowonly",
+        "spec_encode": "cnn14_pool",
+        "embed_dim": 512
+    }
+
+    # load stage 1 checkpoint
+    first_stage_ckpt_path = "/Users/920753844/Diff-Foley/training/open_cavp_main/logs/2024_10_21-14_32_19-lr_8e-4_warmup200_wds_vgg+audioset_cnn14_pretrained_clip_num3_shift_lb8_intra_loss_w1/checkpoints/epoch_latest.pt"
+
+    model = init_first_from_ckpt(first_stage_ckpt_path)
+
+if __name__ == "__main__":
+    main()
