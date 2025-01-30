57d164d3cdad43ebebb42316b15b876acb987c3e
x1_user <psukphranee@gmail.com>
2025-01-29 16:43:44 -0800

    fdsfsd

--- training/stage2_ldm/main.py
+++ training/stage2_ldm/main.py
@@ -179,7 +179,7 @@ class DataModuleFromConfig(pl.LightningDataModule):
         return DataLoader(self.datasets['validation'], batch_size=self.batch_size, num_workers=self.num_workers, worker_init_fn=self.worker_init_fn, shuffle=True)
     
     # Panya Jan 29, 2025. Defining this per chatgpt probably because of deprecated syntax
-    def _test_dataloader(self):
+    def test_dataloader(self):
         return self._test_dataloader()
 
 
