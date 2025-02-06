d03d159c1b643213cc4f4d7dbdb999ea1cac703d
Pan Su <psukphranee@gmail.com>
2025-02-05 21:53:37 -0800

    jb

--- training/stage2_ldm/adm/data/video_spec_dataset.py
+++ training/stage2_ldm/adm/data/video_spec_dataset.py
@@ -176,8 +176,8 @@ class audio_video_spec_fullset_Dataset(torch.utils.data.Dataset):
             video_path2 = self.video_list[random_idx]
 
         # Load the Spec and Feat:
-        print("Panya: __getitem__: ", str(spec_npy_path1))
-        print("Panya: __getitem__: ", str(video_feat_path1))
+        # print("Panya: __getitem__: ", str(spec_npy_path1))
+        # print("Panya: __getitem__: ", str(video_feat_path1))
         spec1, video_feat1 = self.load_spec_and_feat(spec_npy_path1, video_feat_path1)
 
         if flag:
