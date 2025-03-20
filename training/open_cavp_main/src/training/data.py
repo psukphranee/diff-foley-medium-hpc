1e420ee2ebd7df676a1c334e121557b6698dcffd
HPC Desktop SEIC 404 <psukphranee@gmail.com>
2025-03-20 13:51:30 -0700

    added debug statement

--- training/open_cavp_main/src/training/data.py
+++ training/open_cavp_main/src/training/data.py
@@ -2382,6 +2382,9 @@ def cut_video_and_spec_vggsound_audioset_temporal_contrast(video, spec, sample_n
         sample_video_list.append(sample_video.unsqueeze(0))
     
     sample_spec_list = torch.from_numpy(np.concatenate(sample_spec_list, 0))    # sample_num x H x W
+
+    msg = "Panya: []: Attempting to concatenate: {sample_video_list} along dimension 0"
+    logging.info(msg)
     sample_video_list = torch.cat(sample_video_list, 0)                         # sample_num x T x C x H x W
 
     log_str = f"Panya: [cut_video_and_spec_vggsound_audioset_temporal_contrast]: exiting and returning: sample_video_list, sample_spec_list, start_frame, end_frame"
